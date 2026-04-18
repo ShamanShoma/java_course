c20db9a9c7be687528db2099e1a7b46b2f357b17
ShamanShoma <shamanshoma@icloud.com>
2026-04-18 19:58:35 +0500

    Added solution to the leetcode folder

---
+++ src/leetcode/Solution.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            String numStr = String.valueOf(nums[i]);
+
+            if (numStr.length() % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
